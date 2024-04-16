5ef60451fe3b344c35d07408b92b3e728addbd52
Nam Hai <lucas.tran@imt-atlantique.net>
2024-04-16 17:16:07 +0200

    .

--- pages_transitions/default.transition.ts
+++ pages_transitions/default.transition.ts
@@ -7,6 +7,8 @@ export type defaultTransitionProps = {
 
 
 export const defaultFlowOut: FlowFunction<defaultTransitionProps> = (props, resolve, provider) => {
+    const { filterOpen } = useStoreFilter()
+    filterOpen.value = false
     resolve()
 }
 
