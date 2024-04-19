cb965296e5e7248ff30cfe1064689523cad3acc3
Nam Hai <lucas.tran@imt-atlantique.net>
2024-04-19 14:33:13 +0200

    .

--- pages/info.vue
+++ pages/info.vue
@@ -71,7 +71,7 @@ main {
 
 
       &:last-child {
-        // margin-bottom: $main-margin;
+        margin-bottom: calc($main-margin + env(safe-area-inset-bottom));
         height: calc($grid-cell-height + $main-margin * 2 + env(safe-area-inset-bottom) + env(safe-area-inset-top));
       }
     }
