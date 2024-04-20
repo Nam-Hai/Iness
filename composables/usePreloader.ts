42fc93f2af5fcb2568312e50f6643650d6d6d45a
Nam Hai <lucas.tran@imt-atlantique.net>
2024-04-20 12:20:52 +0200

    .

--- composables/usePreloader.ts
+++ composables/usePreloader.ts
@@ -184,6 +184,7 @@ export const usePreloader = createStore(() => {
                         }
                     })
                 })
+                console.log(info);
 
                 return {
                     overview,
