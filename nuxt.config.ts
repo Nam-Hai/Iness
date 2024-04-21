b91d2f2e3457f9e9bcb3c1d3bf12267aeadcfb3f
Nam Hai <lucas.tran@imt-atlantique.net>
2024-04-21 18:56:45 +0200

    .

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -90,7 +90,7 @@ export default defineNuxtConfig({
                     content: "TODO",
                 }
             ],
-            title: "Iness - Portfolio",
+            title: "Ines - Portfolio",
             link: [
                 {
                     rel: "manifest",
